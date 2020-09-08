1f88668e0a8999f17fe46cf9a37fac992cf16db2
mcmullan0 <mcmullan0@gmail.com>
2020-09-08 16:36:32 +0100

    paml-yn00-diversity.sh -Lots of changes involved with retaining INFINITY information where there are dn mutations but no ds mutations.  Also now records if a gene has an incomplete CDS.  This infinity and incpomplete VDS information are now recorded in an info field in the output along with if there was a STOPPolyM

--- shell/paml-yn00-diversity.sh
+++ shell/paml-yn00-diversity.sh
@@ -19,13 +19,11 @@
 RAND=$((1 + RANDOM % 999999))
 # -f takes the directory file
 # -c states that the control file exists already and to use it and run anyway
-# -Q prints scores for genes without any dS varients
 CTL=0
-zeros=0
 kleen=0
 # FYI $USER is assumed to be set to the current user for checking when jobs have finished in the queue
 
-while getopts "f:cQk" opt
+while getopts "f:ck" opt
 do
   case $opt in
     f)
@@ -35,9 +33,6 @@ do
       ;;
     c)
       CTL=1
-      ;;
-    Q)
-      zeros=1
       ;;
     k)
       kleen=1
@@ -50,22 +45,26 @@ if [[ -z ${DIRECTORY_FILE+f} ]]
 then
   echo -ne "\n\n######################################################## paml-yn00-diversity.sh ############################################################\n"
   echo -ne "# Uses a directory of (multi-individual) gene fastas to return average dnds (YN00) for each gene\n"
-  echo -ne "# Can be run on multiple directories (or populaitons)\n"
+  echo -ne "# Can be run on multiple directories (or populations)\n"
   echo -ne "# (Run after vcf2gene.sh)\n"
   echo -ne "# Requires:\n"
   echo -ne "#\t-f a file containing the full path to each population directory\n"
   echo -ne "#\t   Directories will contain a fasta file for each gene with entries for each individual/sequence\n"
   echo -ne "#\t   Remove any trailing '/' from each line\n"
-  echo -ne "#\t-Q a flag that prevents the removal of genes with zero dS SNPs.\n"
-  echo -ne "#\t   This may massivly overinflate omega because of a divistion by zero but could be useful if you want to see these events.\n"
   echo -ne "#\t-c if you don't want this script to print a YN00 control file (if you have one of your own).\n"
   echo -ne "#\t-k if you want to keep all the intermediate phylip and YN00 control files and output.\n"
   echo -ne "# Accepts output from vcf2gene.sh which generates gene fastas from a vcf and a reference fasta genome\n"
   echo -ne "#\tConverts fasta to phylip\n"
-  echo -ne "#\tIdentifies genes with a premature stop codon polymorphism (see POPULATION.stops-present.list) which are not run\n"
+  echo -ne "#\tIdentifies genes with a premature stop codon polymorphism (see POPULATION.fas.stops-present.list) which are not run\n"
+  echo -ne "#\tIdentifies genes with CDS not a multiple of 3 (codon) caused by models that ae 'incomplete CDS' and shortens to 3\n"
   echo -ne "#\tRuns all remaining genes through YN00 and calculates the mean dnds see POPULATION.yn00.mean.txt\n"
   echo -ne "#\t\t<POPULATION> comes from the name of the directory listed in -f\n"
-  echo -ne "#\t\tWhich is expected to appear POPULATION__outputdir\n"
+  echo -ne "#\t\tWhich is expected to appear POPULATION_outputdir\n"
+  echo -ne "#\tInfo field records:\n"
+  echo -ne "#\t\tPolyM\n"
+  echo -ne "#\t\tNoPolyM\n"
+  echo -ne "#\t\tStoPolyM\t-stop present as polymorphism -no YN00 data\n"
+  echo -ne "#\t\tCDSincmp\t-PolyM + incomplete CDS idnetified by VDS not multiple of 3 (not a catch all)\n"
   echo -ne "# Output is stored in present dir\n"
   echo -ne "#######################################################################################################################################\n\n"
   exit 1
@@ -92,13 +91,15 @@ fi
 ### Pangenome: In the case that the individuals with this gene are not present in this analysis
 ### count lines in the fasta to determine whether to use it.
 # If the gene too long fastx_collaper won't work so I do it myself (see LONFAS and FASLEN
+# Added a section to rectify gene length for incomplete CDS
 while read DIRECTORY
 do
   HOLDER=$(echo "${DIRECTORY%_*}" | awk -F '/' '{print $NF}')
   NEWDR="${HOLDER}_yn00"
   mkdir -p "$NEWDR"
+  > ${NEWDR}/${HOLDER}.fas.incomplete.cds.list
   submit-slurm.pl -j phy-mm_${NEWDR}_01 -i "source fastx_toolkit-0.0.13.2
-  ls ${DIRECTORY}/*.fas | awk -F '/' '{print \$NF}' | cut -d '.' -f 1 > ${NEWDR}.fas.list
+  ls ${DIRECTORY}/ | grep '\.fas' | awk -F '/' '{print \$NF}' | sed 's/\.fas//' > ${HOLDER}.fas.list
   cd ${NEWDR}
   while read FAS
   do
@@ -110,7 +111,19 @@ do
       if [[ \"\${FASLEN}\" -lt \"23500\" ]]
       then
         fastx_collapser -i ${DIRECTORY}/\${FAS}.fas | Fasta2Phylip.pl - \${FAS}.temp
-        cat <(sed -n '1p' \${FAS}.temp | awk '{l=\$2-3; print \$1,l}') <(sed '1d' \${FAS}.temp | sed 's/...\$//') | sed \$'s/\t/  /' > \${FAS}.phy
+        REMAIN=\$(( \$FASLEN % 3 ))
+        if [[ \"\$REMAIN\" -eq \"0\" ]]
+        then
+          cat <(sed -n '1p' \${FAS}.temp | awk '{l=\$2-3; print \$1,l}') <(sed '1d' \${FAS}.temp | sed 's/...\$//') | sed \$'s/\t/  /' > \${FAS}.phy
+        elif [[ \"\$REMAIN\" -eq \"1\" ]]
+        then
+          echo \"\${FAS}\" >> ${HOLDER}.fas.incomplete.cds.list
+          cat <(sed -n '1p' \${FAS}.temp | awk '{l=\$2-4; print \$1,l}') <(sed '1d' \${FAS}.temp | sed 's/....\$//') | sed \$'s/\t/  /' > \${FAS}.phy
+        elif [[ \"\$REMAIN\" -eq \"2\" ]]
+        then
+          echo \"\${FAS}\" >> ${HOLDER}.fas.incomplete.cds.list
+          cat <(sed -n '1p' \${FAS}.temp | awk '{l=\$2-5; print \$1,l}') <(sed '1d' \${FAS}.temp | sed 's/.....\$//') | sed \$'s/\t/  /' > \${FAS}.phy
+        fi
         rm \${FAS}.temp
       else
         echo \"\${FAS}.fas looks long, it's \${FASLEN} bp!  Running manual concatenation to haplotypes\"
@@ -134,7 +147,7 @@ do
     else
       echo -e \"${NEWDR} \${FAS}.fas has zero haplotypes in this population/set.\"
     fi
-  done < ../${NEWDR}.fas.list
+  done < ../${HOLDER}.fas.list
   cd ../"
 done < $DIRECTORY_FILE
 
@@ -156,37 +169,33 @@ done
 # These genes won't be run through paml to collect dnds data.
 # However they will be stored in a file called POPULATION.fas.stops-present.list
 
-echo -e "\n\nRunning YN00\nFirst to check for premature stop codons which won't be run through paml to collect dnds data. However they will be stored in POPULATION.fas.stops-present.list and removed from ${NEWDR}.fas.stops-removed.list.\nSecond to collect dnds information\n\n"
-
+echo -e "\n\nRunning YN00\nFirst to check for premature stop codons which won't be run through paml to collect dnds data. However they will be stored in POPULATION.fas.stops-present.list and removed from ${HOLDER}.fas.stops-removed.list.\nSecond to collect dnds information\n\n"
 
 while read DIRECTORY
 do
   HOLDER=$(echo "../${DIRECTORY%_*}" | awk -F '/' '{print $NF}')
   NEWDR="${HOLDER}_yn00"
   # Make a list of genes which I can remove genes with early stop codons from and a list that contains them.
-  cp ${NEWDR}.fas.list ${NEWDR}.fas.stops-removed.list
-  > ${NEWDR}.fas.stops-present.list
+  cp ${HOLDER}.fas.list ${NEWDR}/${HOLDER}.fas.stops-removed.list
+  > ${NEWDR}/${HOLDER}.fas.stops-present.list
   submit-slurm.pl -j yn00-mm_${NEWDR}_02 -i "source paml-4.9
   cd $NEWDR
   while read GENES
   do 
     sed \"s/MM\.sedreplace\.MM/\${GENES}.phy/\" ../yn00.ctl > yn00-run_${HOLDER}-\${GENES}.ctl
-  done < ../${NEWDR}.fas.list
+  done < ../${HOLDER}.fas.list
   for GENES in *.ctl
   do
     GENE=\$(echo \"\${GENES}\" | sed -e \"s/yn00-run_${HOLDER}-//\" -e 's/\.ctl//')
     STOP=\$(yn00 \$GENES | grep 'stop codon' | wc -l)
-    echo \"\$GENE \$STOP\"
     if [[ \"\$STOP\" -eq \"0\" ]]
     then
-      echo \"no stops\"
       BBOYS=\$(wc -l \${GENE}.phy | awk '{n=\$1-1; b=(n*(n-1))/2; l=7+b; print l}')
       grep -A \$BBOYS \"(B) Yang & Nielsen (2000) method\" \${GENE}.phy.yn > \${GENE}.phy.yn.yn00
     else
-      echo \"stops\"
-      grep -v \$GENE ../${NEWDR}.fas.stops-removed.list > MM.stops.removed.MM-\${GENEs}-${NEWDR}
-      mv MM.stops.removed.MM-\${GENEs}-${NEWDR} ../${NEWDR}.fas.stops-removed.list
-      echo \"\${GENE}\" >> ../${NEWDR}.fas.stops-present.list
+      grep -v \$GENE ${HOLDER}.fas.stops-removed.list > MM.stops.removed.MM-\${GENEs}-${HOLDER}
+      mv MM.stops.removed.MM-\${GENEs}-${HOLDER} ${HOLDER}.fas.stops-removed.list
+      echo \"\${GENE}\" >> ${HOLDER}.fas.stops-present.list
     fi
   done
   rm rst rst1 rub 2YN*
@@ -207,7 +216,7 @@ do
   echo -e "${TIMEWAIT} minutes\tThere are $CONTINUE jobs running (YN00)."
 done
 
-echo -e "\nGenerating a table of mean dnds for each gene (ds>0)\n"
+echo -e "\nGenerating a table of mean dnds for each gene (ds>=0)\n"
 # Collect data (mean dnds) from the YN00 output
 # If there was no output print NA
 # If after removal of all of the ds=0 values (causing dnds=99) there are no lines print NA
@@ -216,40 +225,55 @@ while read DIRECTORY
 do
   HOLDER=$(echo "../${DIRECTORY%_*}" | awk -F '/' '{print $NF}')
   NEWDR="${HOLDER}_yn00"
-  > ../${NEWDR}.no-diversity.list
+  > ${NEWDR}/${HOLDER}.no-diversity.list
   submit-slurm.pl -j mean_${NEWDR}_03 -i "cd $NEWDR
-  while read STOPSREMOVED
+  while read ALLGENE
   do
-    NOLINE8=\$(cat \${STOPSREMOVED}.phy.yn.yn00 | wc -l | awk '{k=\$1-8; print k}')
-    echo \"\$STOPSREMOVED\"
-    if [[ \"\$NOLINE8\" -eq \"0\" ]]
+    echo \"\$ALLGENE\"
+    STOPOLYM=\$(grep \$ALLGENE ${HOLDER}.fas.stops-present.list | wc -l)
+    if [[ \"\$STOPOLYM\" -eq \"1\" ]]
     then
-      echo -e \"NA\tNA\tNA\tNA\tNA\tNA\tNA\"
-      echo \${STOPSREMOVED} >> ../${NEWDR}.no-diversity.list
-    elif [[ \"\$NOLINE8\" -gt \"0\" ]] && [[ \"\$zeros\" -eq \"0\" ]]
+      echo -e \"StoPolyM\tNA\tNA\tNA\tNA\tNA\tNA\tNA\"
+    elif [[ \"\$STOPOLYM\" -eq \"0\" ]]
     then
-      tail -n \$NOLINE8 \${STOPSREMOVED}.phy.yn.yn00 | awk '\$11>0' > MM.\${STOPSREMOVED}.phy.yn.yn00.ds-gt0.MM
-      NOLINEMORE=\$(cat MM.\${STOPSREMOVED}.phy.yn.yn00.ds-gt0.MM | wc -l)
-      if [[ \"\$NOLINEMORE\" -eq \"0\" ]]
+      INCMPLTCDS=\$(grep \$ALLGENE $HOLDER.fas.incomplete.cds.list | wc -l)
+      NOLINE8=\$(cat \${ALLGENE}.phy.yn.yn00 | wc -l | awk '{k=\$1-8; print k}')
+      if [[ \"\$NOLINE8\" -eq \"0\" ]]
       then
-        echo -e \"NA\tNA\tNA\tNA\tNA\tNA\tNA\"
-      else
-        awk -v OFS='\t' '{S+=\$3; N+=\$4; t+=\$5; kappa+=\$6; omega+=\$7; dN+=\$8; dS+=\$11} END {print S/NR, N/NR, t/NR, kappa/NR, omega/NR, dN/NR, dS/NR}' MM.\${STOPSREMOVED}.phy.yn.yn00.ds-gt0.MM
+        echo -e \"NoPolyM\tNA\tNA\tNA\tNA\tNA\tNA\tNA\"
+        echo \${ALLGENE} >> ${HOLDER}.no-diversity.list
+      elif [[ \"\$NOLINE8\" -gt \"0\" ]]
+      then
+        tail -n \$NOLINE8 \${ALLGENE}.phy.yn.yn00 > MM.\${ALLGENE}.phy.yn.yn00.ds-eq0.MM
+        if [[ \"\$INCMPLTCDS\" -eq \"1\" ]]
+        then
+          awk -v OFS='\t' -v x='CDSincmp' '{S+=\$3; N+=\$4; t+=\$5; kappa+=\$6; omega+=\$7; dN+=\$8; dS+=\$11} END {print x, S/NR, N/NR, t/NR, kappa/NR, omega/NR, dN/NR, dS/NR}' MM.\${ALLGENE}.phy.yn.yn00.ds-eq0.MM > MM.\${ALLGENE}.phy.yn.yn00.ds-eq0.mean.MM
+        elif [[ \"\$INCMPLTCDS\" -eq \"0\" ]]
+          then
+          awk -v OFS='\t' -v x='PolyM' '{S+=\$3; N+=\$4; t+=\$5; kappa+=\$6; omega+=\$7; dN+=\$8; dS+=\$11} END {print x, S/NR, N/NR, t/NR, kappa/NR, omega/NR, dN/NR, dS/NR}' MM.\${ALLGENE}.phy.yn.yn00.ds-eq0.MM > MM.\${ALLGENE}.phy.yn.yn00.ds-eq0.mean.MM
+        else
+          echo \"Error with incomplete CDS\"
+          exit 1
+        fi
+        MORTHNZEROS=\$(awk '\$8>0' MM.\${ALLGENE}.phy.yn.yn00.ds-eq0.mean.MM | wc -l)
+        MORTHNZERON=\$(awk '\$7>0' MM.\${ALLGENE}.phy.yn.yn00.ds-eq0.mean.MM | wc -l)
+        if [[ \$\"MORTHNZEROS\" -eq \"0\" ]] && [[ \$\"MORTHNZERON\" -gt \"0\" ]]
+        then
+          awk -v OFS='\t' -v I='INFINITY' '{print I,\$2,\$3,\$4,\$5,I\"-\"\$6,\$7,\$8}' MM.\${ALLGENE}.phy.yn.yn00.ds-eq0.mean.MM
+        else
+          cat MM.\${ALLGENE}.phy.yn.yn00.ds-eq0.mean.MM
+        fi
+        rm MM.\${ALLGENE}.phy.yn.yn00.ds-eq0.mean.MM
       fi
-    elif [[ \"\$NOLINE8\" -gt \"0\" ]] && [[ \"\$zeros\" -eq \"1\" ]]
-    then
-      tail -n \$NOLINE8 \${STOPSREMOVED}.phy.yn.yn00 > MM.\${STOPSREMOVED}.phy.yn.yn00.ds-eq0.MM
-      awk -v OFS='\t' '{S+=\$3; N+=\$4; t+=\$5; kappa+=\$6; omega+=\$7; dN+=\$8; dS+=\$11} END {print S/NR, N/NR, t/NR, kappa/NR, omega/NR, dN/NR, dS/NR}' MM.\${STOPSREMOVED}.phy.yn.yn00.ds-eq0.MM
+    else
+      echo \"Error with stop polymorphism\"
+      exit 1
     fi
-  done < ../${NEWDR}.fas.stops-removed.list | paste - - > MM.temp.yn00.out-${NEWDR}.MM
+  done < ../${HOLDER}.fas.list | paste - - > MM.temp.yn00.out-${HOLDER}.MM
   # Add headers and clean up
-  if [[ \"\$zeros\" -eq \"0\" ]]
-  then
-    cat <(echo -e \"Gene\tS\tN\tt\tkappa\tomega\tdN\tdS\") MM.temp.yn00.out-${NEWDR}.MM > ../${HOLDER}.yn00.mean.txt
-  else
-    cat <(echo -e \"Gene\tS\tN\tt\tkappa\tomega\tdN\tdS\") MM.temp.yn00.out-${NEWDR}.MM > ../${HOLDER}.yn00.mean_omega-infinity.txt
-  fi
-  rm MM.temp.yn00.out-${NEWDR}.MM MM.*.phy.yn.yn00.ds-??0.MM
+  cat <(echo -e \"Gene\tInfo\tS\tN\tt\tkappa\tomega\tdN\tdS\") MM.temp.yn00.out-${HOLDER}.MM > ../${HOLDER}.yn00.mean.txt
+  rm MM.temp.yn00.out-${HOLDER}.MM MM.*.phy.yn.yn00.ds-??0.MM
+  mv *.list ../
   cd ../"
 done < $DIRECTORY_FILE
 
@@ -267,7 +291,7 @@ do
 done
 
 
-if [[ "$kleen" -eq 0 ]]
+if [[ "$kleen" -eq "0" ]]
 then
   echo -e "\nYou have chosen to remove phylip and YN00 intermediate files and their directories\n"
   while read DIRECTORY
@@ -276,14 +300,14 @@ then
     NEWDR="${HOLDER}_yn00"
     echo "The directory I will remove is $NEWDR"
     submit-slurm.pl -j remove_${NEWDR}_03 -i "rm -r $NEWDR"
-  done
+  done < $DIRECTORY_FILE
 else
   echo -e "\nYou have chosen to preserve phylip and YN00 intermediate files in their directories\n"
 fi
 
 ## If we are deleting intermediate files await completion of previos jobs
 
-if [[ "$kleen" -eq 0 ]]
+if [[ "$kleen" -eq "0" ]]
 then
   CONTINUE=$(squeue -u "$USER" | grep -c 'MM-remo')
   WAITTIME=1
@@ -296,17 +320,13 @@ then
     CONTINUE=$(squeue -u "$USER" | grep -c 'MM-remo')
     echo -e "${TIMEWAIT} minutes\tThere are $CONTINUE jobs running (removing intermediate files)."
   done
+  rm remove_*.???.slurm
 fi
 
 mkdir paml-yn00-diversity_slurmout
-mv phy-mm*.slurm yn00-mm*.slurm mean_*.slurm *yn00.fas.list *.stops-present.list *.stops-removed.list *.no-diversity.list paml-yn00-diversity_slurmout
-
-if [[ "$kleen" -eq 0 ]]
-then
-  mv remove_*.slurm paml-yn00-diversity_slurmout
-fi
+mv phy-mm*.slurm yn00-mm*.slurm mean_*.slurm *.fas.incomplete.cds.list  *.fas.stops-present.list *.fas.stops-removed.list *.no-diversity.list paml-yn00-diversity_slurmout
 
 echo -ne "\n\n\tRun complete\n\n"
 
-exit
+exit 0
 
